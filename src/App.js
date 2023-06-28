eecb8724078436d70402a6b8c196e3d50fc48b13
Lim Wen Hao <wenhaojshs@gmail.com>
2023-06-28 10:05:42 +0800

    add files

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const blogObj = {
+    title: 'Blog TItle 1',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci numquam laborum vitae beatae molestias quia praesentium nihil illum labore et, consequuntur nesciunt optio expedita laboriosam voluptas ducimus aliquid eligendi enim.'
+  }
+
+  const styles = {
+    borderRadius: '5px',
+    boxShadow: '0 2px 5px #ccc',
+    boxSizing: 'border-box',
+    margin: '16px',
+    padding: '16px'
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={styles}>
+        <h3>{blogObj.title}</h3>
+        <p>{blogObj.description}</p>
+      </div>
+      <div style={styles}>
+        <h3>{blogObj.title}</h3>
+        <p>{blogObj.description}</p>
+      </div>
+      <div style={styles}>
+        <h3>{blogObj.title}</h3>
+        <p>{blogObj.description}</p>
+      </div>
     </div>
   );
 }
