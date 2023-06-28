d587ac2412a4f579b478707da03e1e9155a0941e
Lim Wen Hao <wenhaojshs@gmail.com>
2023-06-28 12:23:39 +0800

    add files

--- src/App.js
+++ src/App.js
@@ -3,33 +3,38 @@ import './App.css';
 
 function App() {
 
-  const blogObj = {
-    title: 'Blog TItle 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci numquam laborum vitae beatae molestias quia praesentium nihil illum labore et, consequuntur nesciunt optio expedita laboriosam voluptas ducimus aliquid eligendi enim.'
-  }
+  const blogArr = [
+    {
+      id: 1,
+      title: 'Blog Title 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci numquam laborum vitae beatae molestias quia praesentium nihil illum labore et, consequuntur nesciunt optio expedita laboriosam voluptas ducimus aliquid eligendi enim.'
+    },
+    {
+      id: 2,
+      title: 'Blog Title 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci numquam laborum vitae beatae molestias quia praesentium nihil illum labore et, consequuntur nesciunt optio expedita laboriosam voluptas ducimus aliquid eligendi enim.'
+    },
+    {
+      id: 3,
+      title: 'Blog Title 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci numquam laborum vitae beatae molestias quia praesentium nihil illum labore et, consequuntur nesciunt optio expedita laboriosam voluptas ducimus aliquid eligendi enim.'
+    }
+  ];
 
-  const styles = {
-    borderRadius: '5px',
-    boxShadow: '0 2px 5px #ccc',
-    boxSizing: 'border-box',
-    margin: '16px',
-    padding: '16px'
-  };
+  const blogCards = blogArr.map((item) => {
+    console.log(item);
+
+    return (
+      <div className = "BlogCard" key={item.id}>
+        <h3>{item.title}</h3>
+        <p>{item.description}</p>
+      </div>
+    );
+  });
 
   return (
     <div className="App">
-      <div style={styles}>
-        <h3>{blogObj.title}</h3>
-        <p>{blogObj.description}</p>
-      </div>
-      <div style={styles}>
-        <h3>{blogObj.title}</h3>
-        <p>{blogObj.description}</p>
-      </div>
-      <div style={styles}>
-        <h3>{blogObj.title}</h3>
-        <p>{blogObj.description}</p>
-      </div>
+      {blogCards}
     </div>
   );
 }
