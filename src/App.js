2adb0d97d2296cd7e3fa2f08720a316707f8e893
Lim Wen Hao <wenhaojshs@gmail.com>
2023-07-03 13:47:00 +0800

    passing function as props

--- src/App.js
+++ src/App.js
@@ -5,41 +5,43 @@ import { isArrayEmpty as isMyArrayEmpty } from './utils';
 
 class App extends Component {
   state = {
-    showBlogs: true
+    showBlogs: true,
+    blogArr: [
+      {
+        id: 1,
+        title: 'Blog Title 1',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci numquam laborum vitae beatae molestias quia praesentium nihil illum labore et, consequuntur nesciunt optio expedita laboriosam voluptas ducimus aliquid eligendi enim.',
+        likeCount: 0
+      },
+      {
+        id: 2,
+        title: 'Blog Title 2',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci numquam laborum vitae beatae molestias quia praesentium nihil illum labore et, consequuntur nesciunt optio expedita laboriosam voluptas ducimus aliquid eligendi enim.',
+        likeCount: 0
+      },
+      {
+        id: 3,
+        title: 'Blog Title 3',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci numquam laborum vitae beatae molestias quia praesentium nihil illum labore et, consequuntur nesciunt optio expedita laboriosam voluptas ducimus aliquid eligendi enim.',
+        likeCount: 0
+      }
+    ]
   }
 
-  blogArr = [
-    {
-      id: 1,
-      title: 'Blog Title 1',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci numquam laborum vitae beatae molestias quia praesentium nihil illum labore et, consequuntur nesciunt optio expedita laboriosam voluptas ducimus aliquid eligendi enim.'
-    },
-    {
-      id: 2,
-      title: 'Blog Title 2',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci numquam laborum vitae beatae molestias quia praesentium nihil illum labore et, consequuntur nesciunt optio expedita laboriosam voluptas ducimus aliquid eligendi enim.'
-    },
-    {
-      id: 3,
-      title: 'Blog Title 3',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci numquam laborum vitae beatae molestias quia praesentium nihil illum labore et, consequuntur nesciunt optio expedita laboriosam voluptas ducimus aliquid eligendi enim.'
-    }
-  ];
+  onLikeBtnClick = (pos) => {
+    const updatedBlogList = this.state.blogArr;
+    const updatedBlogObj = updatedBlogList[pos];
 
-  blogCards = isMyArrayEmpty(this.blogArr) ? [] : this.blogArr.map((item, pos) => {
-    // console.log(item);
+    updatedBlogObj.likeCount += 1;
+    updatedBlogList[pos] = updatedBlogObj;
 
-    return (
-      <BlogHomePageCard className='Blog' key={pos} title={item.title} description={item.description} id={item.id}/>
-      // <div className = "BlogCard" key={item.id}>
-      //   <h3>{item.title}</h3>
-      //   <p>{item.description}</p>
-      // </div>
-    );
-  });
+    this.setState({blogArr: updatedBlogList});
+
+    console.log(updatedBlogObj);
+  }
 
   onHideBtnClick = () => {
-    let updatedState = !this.state.showBlogs;
+    // let updatedState = !this.state.showBlogs;
     this.setState((prevState, prevProps) => {
       return {showBlogs: !prevState.showBlogs}
     });
@@ -49,11 +51,26 @@ class App extends Component {
 
   render(){
     console.log('Render Called');
+    console.log(this.state);
+
+    
+    const blogCards = isMyArrayEmpty(this.state.blogArr) ? [] : this.state.blogArr.map((item, pos) => {
+      // console.log(item);
+
+      return (
+        <BlogHomePageCard className='Blog' key={pos} title={item.title} description={item.description} likeCount={item.likeCount} id={item.id} onLikeBtnClick={() => this.onLikeBtnClick(pos)}/>
+        // <div className = "BlogCard" key={item.id}>
+        //   <h3>{item.title}</h3>
+        //   <p>{item.description}</p>
+        // </div>
+      );
+    });
+
     return (
       <div className="App">
         <button onClick={this.onHideBtnClick}>{this.state.showBlogs ? 'Hide List' : 'Show List'}</button>
         <br></br>
-        {this.state.showBlogs ? this.blogCards : null}
+        {this.state.showBlogs ? blogCards : null}
       </div>
     );
   }
--- src/BlogCard.js
+++ src/BlogCard.js
@@ -4,11 +4,21 @@ import { dumpLogs } from './utils';
 import classes from './BlogCard.module.css';
 
 const BlogCard = (props) => {
+
+    // onLikeBtnClick = () => {
+    //     this.setState((prevState, prevProp) => {
+    //         return {likeCount: prevState.likeCount + 1}
+    //     });
+    // }
+
     dumpLogs(props);
     return(
         <div className = {classes.NewBlogCard}>
             <h3>{props.title}</h3>
             <p>{props.description}</p>
+
+            <p>Like Count: <span className = {classes.LikeCount}>{props.likeCount}</span></p>
+            <button onClick={props.onLikeBtnClick}>Like</button>
         </div>
     );
 };
